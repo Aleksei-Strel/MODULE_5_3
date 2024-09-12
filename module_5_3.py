36a7b36040a6b7fc5bbb2597ce613083e55b7c83
Aleksei-Strel <an@strelnikov-mail.ru>
2024-09-12 13:36:25 +0400

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,68 @@
+# Основа из предыдущей задачи
+class HOUSE:
+    def __init__(self, name, numb_f):
+        self.name = name
+        self.number_of_floor = numb_f
+
+    def __len__(self):
+        return self.number_of_floor
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floor}.'
+# Создаём условия по дополнению класса House следующими специальными методами:
+    def __eq__(self, other):    # 1
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor == other.number_of_floor
+
+    def __lt__(self, other):    # 2
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor < other.number_of_floor
+
+    def __le__(self, other):
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor <= other.number_of_floor
+
+    def __gt__(self, other):
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor > other.number_of_floor
+
+    def __ge__(self, other):
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor >= other.number_of_floor
+
+    def __ne__(self, other):
+        if isinstance(other.number_of_floor, int) and isinstance(other, HOUSE):
+            return self.number_of_floor != other.number_of_floor
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floor = self.number_of_floor + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floor += value
+        return self
+
+# Вывод результатов по условиям задачи:
+
+h1 = HOUSE('Скала', 28)
+h2 = HOUSE('Хрущ', 5)
+
+print(h1)
+print(h2)
+# 1
+print(h1 == h2)
+# 3
+print(h1.__add__(10))
+print(h1 == h2)
+print(h1.__add__(10))
+print(h2.__add__(10))
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 != h2)
